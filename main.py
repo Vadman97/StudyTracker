aa7b860641e091e8db815f35eaafdf4e65523b2a
Vadim Korolik <korolik@usc.edu>
2016-04-06 13:38:08 -0700

    adding bootstrap example, starting frontend

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	print str(url_for('static', filename='bootstrap/css/bootstrap.css'))
 	return 'Index Page'
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
 
 @app.route('/test/')
 @app.route('/test/<name>')
 def test(name=None):
 	return render_template('test.html', name=name)
+
+@app.route('/init/')
+def init():
+	return render_template('initialize.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
