792ddbb8e76063f8508b1b0de990846abcf62bb3
Vadim Korolik <korolik@usc.edu>
2016-04-13 00:59:37 -0700

    Working on submission for done page

--- main.py
+++ main.py
@@ -15,47 +15,71 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Experiment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # hash = db.Column(db.String(64))
-    timeGroup = db.Column(db.Time())
-    annotatorName = db.Column(db.String(64))
-    group = db.Column(db.String(10))
-    outlier = db.Column(db.String(1))
-    friendship = db.Column(db.Integer)
-    notes = db.Column(db.String(2048))
+	id = db.Column(db.Integer, primary_key=True)
+	# hash = db.Column(db.String(64))
+	timeGroup = db.Column(db.Time())
+	annotatorName = db.Column(db.String(64))
+	group = db.Column(db.String(10))
+	outlier = db.Column(db.String(1))
+	friendship = db.Column(db.Integer)
+	notes = db.Column(db.String(2048))
 
-    def __init__(self, timeGroup, annotatorName, group, outlier, friendship, notes):
-    	# m = hashlib.sha512()
-    	# m.update(str(id))
-    	# self.hash = m.hexdigest()
+	def __init__(self, timeGroup, annotatorName, group, outlier, friendship, notes):
+		# m = hashlib.sha512()
+		# m.update(str(id))
+		# self.hash = m.hexdigest()
 
-    	self.timeGroup = datetime.datetime.strptime(timeGroup, "%I:%M %p").time()
-    	self.annotatorName = annotatorName
-    	self.group = group
-    	self.outlier = outlier
-    	self.friendship = friendship
-    	self.notes = notes
+		self.timeGroup = datetime.datetime.strptime(timeGroup, "%I:%M %p").time()
+		self.annotatorName = annotatorName
+		self.group = group
+		self.outlier = outlier
+		self.friendship = friendship
+		self.notes = notes
 
-    def __repr__(self):
-    	return str(self.id)
+	def __repr__(self):
+		return str(self.id)
 
 class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    gender = db.Column(db.String(1))
-    age = db.Column(db.Integer)
+	id = db.Column(db.Integer, primary_key=True)
+	gender = db.Column(db.String(1))
+	age = db.Column(db.Integer)
 
-    experimentID = db.Column(db.Integer, db.ForeignKey('experiment.id'))
-    experiment = db.relationship('Experiment', backref=db.backref('people', lazy='dynamic'))
+	experimentID = db.Column(db.Integer, db.ForeignKey('experiment.id'))
+	experiment = db.relationship('Experiment', backref=db.backref('people', lazy='dynamic'))
 
-    def __init__(self, gender, age, experiment):
-        self.gender = gender
-        self.age = age
-        self.experiment = experiment
+	def __init__(self, gender, age, experiment):
+		self.gender = gender
+		self.age = age
+		self.experiment = experiment
 
-    def __repr__(self):
-        return '<Person ID %r>' % self.id
+	def __repr__(self):
+		return str(self.id)
+
+class PostExperimentData(db.Model):
+	id = db.Column(db.Integer, primary_key=True)	
+	experimentID = db.Column(db.Integer, db.ForeignKey('experiment.id'))
+	experiment = db.relationship('Experiment', backref=db.backref('postResponses', lazy='dynamic'))
+
+	finalTime = db.Column(db.Time)
+	rank = db.Column(db.String(32))
+	solvedMaze = db.Column(db.String(3))
+	#TODO if not solved, string for where they got
+	notes = db.Column(db.String(2048))
+
+	def __init__(self, finalTime, rank, solvedMaze, notes):
+		self.finalTime = finalTime
+		self.rank = rank
+		self.solvedMaze = solvedMaze
+		self.notes = notes
+
+	def __repr__(self):
+		return str(self.id)
 
 db.create_all()
+
+@app.route('/static/<path:path>')
+def send_js(path):
+	return send_from_directory('static', path)
 
 @app.route('/')
 def index():
@@ -65,15 +89,6 @@ def index():
 	for u in exps:
    		users.update({u: u.__dict__})
 	return render_template('index.html', exps=users)
-
-@app.route('/static/<path:path>')
-def send_js(path):
-    return send_from_directory('static', path)
-
-@app.route('/test/')
-@app.route('/test/<name>')
-def test(name=None):
-	return render_template('test.html', name=name)
 
 @app.route('/init/')
 def init():
@@ -93,9 +108,9 @@ def experiment(experimentID=None):
 
 	return render_template('experiment.html', experiment=experimentID)
 
-@app.route('/done/')
-def done():
-	return render_template('postExperiment.html')
+@app.route('/done/<int:experimentID>')
+def done(experimentID=None):
+	return render_template('postExperiment.html', experiment=experimentID)
 
 @app.route('/setupExperiment/', methods=['POST'])
 def setupExperiment():
@@ -118,4 +133,4 @@ def setupExperiment():
 	return redirect(url_for('index'), code=302)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+	app.run(host='0.0.0.0', debug=True, port=5000)
